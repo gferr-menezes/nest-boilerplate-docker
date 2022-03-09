88cb0fdb77ca27716fe4bcc25573de80b2fab206
Guilherme Ferreira <guilherme.menezes@zylix.com.br>
2022-03-09 12:59:05 -0300

    feat: dockerize app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    // logger,
+  });
+
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
